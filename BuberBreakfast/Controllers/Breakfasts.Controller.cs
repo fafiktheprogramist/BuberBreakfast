b5a7b99502b049b4c698fd12ca5e7269432a2b70
fafiktheprogramist <glowaczra@gmail.com>
2024-01-31 18:24:44 +0100

    created underlay for saving breakfast to a DB

--- BuberBreakfast/Controllers/Breakfasts.Controller.cs
+++ BuberBreakfast/Controllers/Breakfasts.Controller.cs
@@ -1,4 +1,5 @@
 using BuberBreakfast.Contracts.Breakfast;
+using BuberBreakfast.Models;
 using Microsoft.AspNetCore.Mvc;
 
 namespace BuberBreakfast.Controllers;
@@ -7,10 +8,35 @@ namespace BuberBreakfast.Controllers;
 [Route("[controller]")]
 public class BreakfastsController : ControllerBase
 {
-[HttpPost("")]
+[HttpPost]
 public IActionResult CreateBreakfast(CreateBreakfastRequest request)
 {
-    return Ok(request);
+    var breakfast = new Breakfast(
+        Guid.NewGuid(),
+        request.Name,
+        request.Description,
+        request.StartDateTime,
+        request.EndDateTime,
+        DateTime.UtcNow,
+        request.Savory,
+        request.Sweet);
+        // Todo: Save it to DB
+
+        //Manual mapping to a response : UNDER 
+        var response = new BreakfastResponse(
+            breakfast.Id,
+            breakfast.Name,
+            breakfast.Description,
+            breakfast.StartDateTime,
+            breakfast.EndDateTime,
+            breakfast.LastModifiedDateTime,
+            breakfast.Savory,
+            breakfast.Sweet);
+
+    return CreatedAtAction(
+        actionName: nameof(GetBreakfast), 
+        routeValues: new {id = breakfast.Id}, 
+        value: response);
 }
 
 [HttpGet("{id:guid}")]
